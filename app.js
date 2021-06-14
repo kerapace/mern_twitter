142c706d141c0f1eb76158343bbe276a893bb338
kerapace <jhill01@manhattan.edu>
2021-06-14 00:14:52 -0400

    updated for heroku push

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 5000;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.js'));
+  })
+}
+
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
